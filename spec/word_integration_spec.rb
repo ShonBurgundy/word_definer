416c40ac80f50cecb52364d6807ac81bceb0edc0
shonburgundy <seanbeloussow@yahoo.com>
2020-05-15 17:15:18 -0700

    pass integration_spec test

--- spec/word_integration_spec.rb
+++ spec/word_integration_spec.rb
@@ -1,5 +1,26 @@
   
-# require('capybara/rspec')
-# require('./app')
-# Capybara.app = Sinatra::Application
-# set(:show_exceptions, false)
+require('capybara/rspec')
+require('./app')
+Capybara.app = Sinatra::Application
+set(:show_exceptions, false)
+
+describe('create a word path', {:type => :feature}) do
+  it('creates a word and then goes to the word page') do
+    visit('/')
+    click_on('Add a new word')
+    fill_in('word_name', :with => 'fun')
+    click_on('Click Here')
+    expect(page).to have_content('fun')
+  end
+end
+
+describe('create an artist path', {:type => :feature}) do
+  it('creates an artist and then goes to the artist page') do
+    word = Word.new({:name => "fun", :id => nil})
+    word.save
+    visit("/words/#{word.id}")
+    fill_in('clarif', :with => 'joy')
+    click_on('Add Definition')
+    expect(page).to have_content("joy")
+  end
+end
